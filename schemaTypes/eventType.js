37496206748c88173e3e7be988786931ead36a66
Pratik Kuril <pratikkuril16@gmail.com>
2025-04-21 22:27:19 +0530

    Add various fields

--- schemaTypes/eventType.js
+++ schemaTypes/eventType.js
@@ -9,5 +9,46 @@ export const eventType = defineType({
       name: 'name',
       type: 'string',
     }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+    }),
+    defineField({
+      name: 'eventType',
+      type: 'string',
+    }),
+    defineField({
+      name: 'date',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'doorsOpen',
+      type: 'number',
+    }),
+    defineField({
+      name: 'venue',
+      type: 'reference',
+      to: [{type: 'venue'}],
+    }),
+    defineField({
+      name: 'headline',
+      type: 'reference',
+      to: [{type: 'artist'}],
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'details',
+      type: 'array',
+      of: [{type: 'block'}], // this is a rich text field
+    }),
+    defineField({
+      name: 'tickets',
+      type: 'url',
+    }),
   ],
 })
+
+// defineType and defineField are optional, we can make this simply by using objects but they help us by suggestions and auto completes.
