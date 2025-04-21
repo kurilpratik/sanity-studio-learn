bd3805e552af6ef52f338091514161b074c701a3
Pratik Kuril <pratikkuril16@gmail.com>
2025-04-22 02:17:57 +0530

    Add a custom input component

---
+++ schemaTypes/components/DoorsOpenInput.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {NumberInputProps, useFormValue} from 'sanity'
+import {Stack, Text} from '@sanity/ui'
+
+function subtractMinutesFromDate(date: string, minutes: number) {
+  return new Date(new Date(date).getTime() - minutes * 60000)
+}
+
+export function DoorsOpenInput(props: NumberInputProps) {
+  const date = useFormValue(['date']) as string | undefined
+  return (
+    <Stack space={3}>
+      {props.renderDefault(props)}
+      {typeof props.value === 'number' && date ? (
+        <Text size={1}>
+          Doors open{'   '}
+          {subtractMinutesFromDate(date, props.value).toLocaleTimeString(undefined, {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          })}
+        </Text>
+      ) : null}
+    </Stack>
+  )
+}
