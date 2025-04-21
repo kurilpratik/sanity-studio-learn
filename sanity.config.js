81348822334766742ef613296660222485b9416f
Pratik Kuril <pratikkuril16@gmail.com>
2025-04-22 02:47:18 +0530

    Add custom document pane for artist which displays their events

--- sanity.config.js
+++ sanity.config.js
@@ -4,6 +4,7 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 import {structure} from './structure'
+import {defaultDocumentNode} from './structure/defaultDocumentNode'
 
 export default defineConfig({
   name: 'default',
@@ -15,6 +16,7 @@ export default defineConfig({
   plugins: [
     structureTool({
       structure, // Use our own custome structure that we defined in structure/index.ts
+      defaultDocumentNode,
     }),
     visionTool(),
   ],
